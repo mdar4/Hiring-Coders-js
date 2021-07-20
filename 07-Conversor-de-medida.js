0ebce063d1b01e07cf88d6f1fb7bea685d851f8b
mdar4 <darasilva2391@gmail.com>
2021-07-20 20:33:50 -0300

    exercício Conversor de medidas

---
+++ 07-Conversor-de-medida.js
@@ -0,0 +1,7 @@
+// Escreva um programa que receba um valor em metros e o exiba no console convertido em milímetros.
+
+function conversor(valor){
+    return  valor * 1000;
+}
+let resultado = conversor(1);
+console.log(`O valor digitado é igual a ${resultado}mm`)
