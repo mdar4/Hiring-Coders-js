049a5c653605a5b3c0bb1cfbbb5773aa4bd77a51
mdar4 <darasilva2391@gmail.com>
2021-07-20 19:54:02 -0300

    exercício de concatenação e interpolação

---
+++ 02-ConcatenarXInterpolar.js
@@ -0,0 +1,8 @@
+//Vamos criar variaveis usando let, const e var, atribuir strings e vamos concatenar e interpolar.
+
+const nome = 'Dara';
+let idade = 'tenho 20 anos';
+var reside = 'moro em Peruíbe';
+
+// console.log('Eu sou a ' + nome + ' ' + idade + 'e'+ ' ' + reside); (Concatenação)
+console.log(`Eu sou a ${nome}, ${idade} e ${reside}`); // (interpolação)
