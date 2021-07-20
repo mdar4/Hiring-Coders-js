d8f4dcf0702c94d0265f63edd7ca50137c62303f
mdar4 <darasilva2391@gmail.com>
2021-07-20 19:53:16 -0300

    Exercício de váriaveis

---
+++ 01-variáveis.js
@@ -0,0 +1,5 @@
+// Vamos criar variaveis usando let, const e var, atribuir valores, identificar comportamentos e saídas quando tentarmos recuperar. 
+
+var idade = 20; // é mutavel e pode ser chamada a qualquer momento
+let peso = 48; // let é mutável  e só pode ser chamada em seu escopo
+const nome = 'Dara'; // const não é mutável ensó pode ser chamada dentro de seu escopo
