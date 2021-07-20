68d74e14a7682b21d4b2afbf031801fb990f142d
mdar4 <darasilva2391@gmail.com>
2021-07-20 20:22:59 -0300

    exercício Desapega

---
+++ 06-Desapega.js
@@ -0,0 +1,6 @@
+// Crie um programa que me diga quanto cobrar para vender um notebook usado: o seu custo foi R$3000, por quanto devo vendê-lo descontando 25%?
+
+let desconto = (3000 * 25) /100;
+let venda = 3000 - desconto;
+
+console.log(venda);
