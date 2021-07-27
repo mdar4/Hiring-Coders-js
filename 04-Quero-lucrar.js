0a3cceec3d65e3ee5fb85ff948e5a1d7d700c52a
Dara Fontoura <81888608+mdar4@users.noreply.github.com>
2021-07-27 12:52:55 -0300

    Update 04-Quero-lucrar.js

--- 04-Quero-lucrar.js
+++ 04-Quero-lucrar.js
@@ -1,8 +1,9 @@
 // Crie um programa que me diga como lucrar nesta situação: quero vender uma bicicleta que 60% do seu valor é R$300, por quanto devo vendê-la para lucrar 25%?
 
-let compra = 300;
-let calculo = (compra * 60) / 100;
-let lucro = (calculo * 25) /100;
-let venda = calculo + lucro;
-
-console.log(venda);
+function vender(compra){
+  let calculo = (compra * 60) / 100;
+  let lucro = (calculo * 25) /100;
+  let venda = calculo + lucro;
+  console.log(venda);
+}
+vender(500)
